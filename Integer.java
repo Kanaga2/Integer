e98d234c7922e6475eb3b09fb8259d0de6d65ed2
Kanaga2 <priyakanaga112@gmail.com>
2017-07-03 21:57:51 +0530

    Create Integer.java

---
+++ Integer.java
@@ -0,0 +1,13 @@
+# Integer
+import java.io.*;
+import java.util.*;
+public class Integer
+{
+public static void main(String args[])throws IOException
+{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+String num="23";
+Integer k=Integer.parseInt(num);
+System.out.println(k);
+}
+}
